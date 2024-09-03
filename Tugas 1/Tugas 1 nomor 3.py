af44e06ea3c12ce49f158ed5ffab80ad9902941e
ilena031 <5054241003@student.its.ac.id>
2024-09-03 23:28:05 +0700

    Delete Tugas 1 directory

--- Tugas 1/Tugas 1 nomor 3.py
+++
@@ -1,9 +0,0 @@
-#to enter the time 
-timehour = int (input ("how long its started? (in hour) "))
-timeminute = int (input ("how long its satarted? (in minute)"))/60
-time= timehour+timeminute
-#to count the temprature
-ExactTemp = round(((4*time**2)/(time+2))-20,2)
-print ("Your temprature is", ExactTemp, "°C")
-
-
